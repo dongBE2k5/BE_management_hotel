69e648178ba3ee8a61be449c41e991da8d226af0
DoNguyenTai <donguyentai05@gmail.com>
2025-09-24 10:37:34 +0700

    create entities

---
+++ src/main/java/tdc/vn/managementhotel/enums/TypeRoom.java
@@ -0,0 +1,11 @@
+package tdc.vn.managementhotel.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+public enum TypeRoom {
+    DON,
+    DOI,
+    GIA_DINH
+}
